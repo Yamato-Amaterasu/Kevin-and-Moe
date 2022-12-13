f81b64369b53295d495b1cf50715fac49418247e
Mohamad Ayyad <hamoodaayyad@gmail.com>
2022-12-13 12:43:58 -0600

    added user router

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require("express");
 const app = express();
 PORT = 2003;
 
+app.use("/users", require("./routes/users"));
+
 app.listen(PORT, () => console.log(`server is up on port ${PORT}`));
